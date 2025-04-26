07593b5eaf271c8f2b2146de40b60730bdfc2e81
Tanusha Goswami <39588997+Tango2503@users.noreply.github.com>
2025-04-26 19:30:31 +0100

    Update page.js

--- src/app/sender/page.js
+++ src/app/sender/page.js
@@ -4,7 +4,7 @@ export default function Sender() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-8 text-center">
       <h1 className="text-3xl font-bold mb-4"> Send Your Thought 🌸</h1>
-      <p className="text-md mb-8"> Hey. No pressure to respond. Just know you're on someone's mind. </p>
+      <p className="text-md mb-8"> Hey. No pressure to respond. Just know you&apos;re on someone&apos;s mind. </p>
 
       <Link href="/receiver" className="text-blue-500 underline">Share It</Link>
     </main>
