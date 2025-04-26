f2d9169c55a5ddb195f8a961d117daf2681da903
Tanusha Goswami <39588997+Tango2503@users.noreply.github.com>
2025-04-26 18:26:15 +0000

    Changed Copy

--- src/app/receiver/page.js
+++ src/app/receiver/page.js
@@ -3,8 +3,8 @@ import Link from 'next/link';
 export default function Receiver() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-8 text-center">
-      <h1 className="text-3xl font-bold mb-4">You Received a Thought 🌼</h1>
-      <p className="text-md mb-8">Someone thought of you and sent a little message. 💛</p>
+      {/*<h1 className="text-3xl font-bold mb-4">You Received a Thought 🌼</h1> */}
+      <p className="text-md mb-8"> Someone wanted you to know: You are quietly loved today. 🌸 </p>
 
       <Link href="/" className="text-blue-500 underline">Send Your Thought</Link>
     </main>
