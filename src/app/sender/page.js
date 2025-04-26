cd29ee027695ff3b1f1b513c6ad6027639e1d1f5
Tanusha Goswami <39588997+Tango2503@users.noreply.github.com>
2025-04-26 16:51:54 +0000

    Adding escape characters

--- src/app/sender/page.js
+++ src/app/sender/page.js
@@ -4,7 +4,7 @@ export default function Sender() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-8 text-center">
       <h1 className="text-3xl font-bold mb-4">Compose Your Thought 🌸</h1>
-      <p className="text-md mb-8">Write a small message you'd like to share with someone special.</p>
+      <p className="text-md mb-8">Write a small message you&apos;d like to share with someone special.</p>
 
       <Link href="/receiver" className="text-blue-500 underline">Share It</Link>
     </main>
