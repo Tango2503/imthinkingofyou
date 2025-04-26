2f16985333038a792e33f842e481fb7efe744e74
Tanusha Goswami <39588997+Tango2503@users.noreply.github.com>
2025-04-26 19:33:02 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-8 text-center">
-      <h1 className="text-4xl font-bold mb-4"> sometimes you just want someone to know you're thinking of them. no pressure, no reply needed. 🌱</h1>
+      <h1 className="text-4xl font-bold mb-4"> sometimes you just want someone to know you&apos;re thinking of them. no pressure, no reply needed. 🌱</h1>
       {/* <p className="text-lg mb-8">A gentle space to send a thought to someone you care about.</p> */}
 
       <Link href="/sender" className="text-blue-500 underline">Send a Thought</Link>
